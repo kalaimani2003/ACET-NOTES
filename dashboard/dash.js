03fc348c1954f67672d07ca26fe841a6c1432e67
prabha haran <hprabha72@gmail.com>
2024-10-13 12:05:46 +0530

    new files

---
+++ dashboard/dash.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+    
+    $("#toggler").click(function () {
+        $("#menu").toggle()
+    })
+    /* for dropdown in dashboard */
+    $("#dropdown2").click(function () {
+        $("#drop-menu2").toggle()
+    })
+     $("#dropdown3").click(function () {
+        $("#drop-menu3").toggle()
+    })
+    $("#dropdown4").click(function () {
+        $("#drop-menu4").toggle()
+    })
+    $("#dropdown1").click(function () {
+        $("#drop-menu1").toggle()
+    })
+   
+ $("#sem-1").click(function(){
+    $("#first-sem").toggle()
+   
+    
+ })
+ 
+
+ $("#sem-2").click(function(){
+    $("#second-sem").toggle()
+    
+ })
+
+ $("#sem-3").click(function(){
+    $("#third-sem").toggle()
+    
+ })
+ $("#sem-4").click(function(){
+    $("#fourth-sem").toggle()
+    
+ })
+ $("#sem-5").click(function(){
+    $("#fifth-sem").toggle()
+    
+ })
+ $("#sem-6").click(function(){
+    $("#sixth-sem").toggle()
+    
+ })
+ $("#sem-7").click(function(){
+    $("#seventh-sem").toggle()
+    
+ })
+
+
+})
